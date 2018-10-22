083e03efd3acc24d971068eccf906aee7b3b8596
charlotteasencio <charlotte.e.asencio@gmail.com>
2018-10-22 15:28:46 -0400

    footer fixes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,10 +6,14 @@ export class Footer extends Component {
     render() {
         return (
             <div className = "footer">
+                <div className="topRow">
                 <p><a href="mailto:webmaster@example.com">payitforward@gmail.com</a></p>
                 <p>phone: 555-555-555</p>
                 <p>Address: 320 E 9th St, Charlotte, NC 28202</p>
+                </div>
+                <div className="bottomRow">
                 <p className="cp">&copy;2019</p>
+                </div>
             </div>
         );
     }
